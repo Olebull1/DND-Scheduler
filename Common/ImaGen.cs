ba49c9ebf15419ee8c4d84bc994afc9bffa05491
Matt Rusnock <matthew.rusnock@gmail.com>
2021-07-03 21:03:51 -0400

    Created the first cut for the function for writing the day to the base template.

--- Common/ImaGen.cs
+++ Common/ImaGen.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Text;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+//using System.Text;
 
 namespace DND_Scheduler.Common
 {
@@ -11,27 +13,34 @@ namespace DND_Scheduler.Common
         {
 
         }
-        public static string DateToCalImage(Date d)
+        public static int WriteDayNameToImage(Date d, string fileName)//(Date d)
         {
-            Bitmap myBitmap = new Bitmap(@"E:\calendar\calendar-blank.bmp");
-            Graphics g = Graphics.FromImage(myBitmap);
+            // Draw vertical string test
+            // Load image 
+            Bitmap monday = new Bitmap(@"C:\Users\Matt\Desktop\Calender\bmps\BlankDayTemplate2.bmp");
+            Graphics g = Graphics.FromImage(monday);
+            // Create text
+            String date = "MONDAY | 07/12/2021";
+            // Create font and brush
+            Font drawFont = new Font("Bebas Neue", 14);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+            // Create points for draw locations
+            Rectangle rect2 = new Rectangle(0,10,80,500);
+            // Set format
+            StringFormat drawFormat = new StringFormat();
+            drawFormat.FormatFlags = StringFormatFlags.DirectionVertical;
+            drawFormat.Alignment = StringAlignment.Near;
+            drawFormat.LineAlignment = StringAlignment.Near;
 
-            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
-            g.DrawString("My\nText",
-                         new Font("Tahoma", 20),
-                         Brushes.White,
-                         new PointF(0, 0));
-
-            StringFormat strFormat = new StringFormat();
-            strFormat.Alignment = StringAlignment.Center;
-            strFormat.LineAlignment = StringAlignment.Center;
-
-            g.DrawString("Fat\n Cocks\n Cocks",
-                         new Font("Tahoma", 20), Brushes.Red,
-                         new RectangleF(0, 0, 500, 500),
-                         strFormat);
-            myBitmap.Save(@"E:\calendar\calendar-edit.bmp");
-            return "";
+            // Draw to image
+            g.TextRenderingHint = TextRenderingHint.AntiAlias;
+            g.DrawString(date, drawFont, drawBrush, rect2, drawFormat);
+            // Rotate the image
+            // Save new image
+            monday.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            monday.Save(@"C:\Users\Matt\Desktop\Calender\bmps\" + fileName);//, ImageFormat.Png);
+    
+            return 1;
         }
         public static string GenerateWeekFromDateTime(DateTime dt)
         {
